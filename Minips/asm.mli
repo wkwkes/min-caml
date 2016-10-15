5a0f0d2c54b867222f6133a979277077e21900ca
wkwkes <oshi.hiroki@gmail.com>
2016-10-15 22:53:01 +0900

    modify

--- Minips/asm.mli
+++ Minips/asm.mli
@@ -1,34 +1,35 @@
-type opr = Id.t * Id.t * int
-type opi = Id.t * int
-type opj = int
-type t = 
-  | Ans of exp
-  | Let of (Id.t * Type.t) * exp * t
+type id_or_imm = V of Id.t | C of int
+type t = Ans of exp | Let of (Id.t * Type.t) * exp * t
 and exp =
-  | Sw of opi
-  | Lw of opi
-  | Ori of opi
-  | Andi of opi
-  | Addi of opi
-  | Add of opr
-  | Slt of opr
-  | Sub of opr
-  | And of opr
-  | Or of opr
-  | Xor of opr
-  | Sll of opr
-  | Sra of opr
-  | Clear
+    Nop
+  | Li of int
+  | FLi of Id.l
+  | SetL of Id.l
+  | Mr of Id.t
+  | Neg of Id.t
+  | Add of Id.t * id_or_imm
+  | Sub of Id.t * id_or_imm
+  | Sll of Id.t * id_or_imm
+  | Lw of Id.t * id_or_imm
+  | Sw of Id.t * Id.t * id_or_imm
+  | FMr of Id.t
+  | FNeg of Id.t
+  | FAdd of Id.t * Id.t
+  | FSub of Id.t * Id.t
+  | FMul of Id.t * Id.t
+  | FDiv of Id.t * Id.t
+  | Lfd of Id.t * id_or_imm
+  | Stfd of Id.t * Id.t * id_or_imm
   | Comment of string
-  | IfEq of Id.t * Id.t * t * t
-  | IfNe of Id.t * Id.t * t * t
-  | IfEqL of Id.t * Id.t * t * t
-  | IfNeL of Id.t * Id.t * t * t
+  | IfEq of Id.t * id_or_imm * t * t
+  | IfLE of Id.t * id_or_imm * t * t
+  | IfGE of Id.t * id_or_imm * t * t
+  | IfFEq of Id.t * Id.t * t * t
+  | IfFLE of Id.t * Id.t * t * t
   | CallCls of Id.t * Id.t list * Id.t list
   | CallDir of Id.l * Id.t list * Id.t list
   | Save of Id.t * Id.t
   | Restore of Id.t
-
 type fundef = {
   name : Id.l;
   args : Id.t list;
@@ -36,19 +37,21 @@ type fundef = {
   body : t;
   ret : Type.t;
 }
-
 type prog = Prog of (Id.l * float) list * fundef list * t
+val fletd : Id.t * exp * t -> t
 val seq : exp * t -> t
 val regs : string array
+val fregs : string array
 val allregs : string list
+val allfregs : string list
 val reg_cl : string
 val reg_sw : string
-val reg_jl : string
-val reg_sp : string
+val reg_fsw : string
 val reg_hp : string
+val reg_sp : string
+val reg_tmp : string
 val is_reg : string -> bool
-val remove_and_uniq : S.t -> S.elt list -> S.elt list
-val fv_exp : exp -> S.elt list
+
 val fv : t -> S.elt list
 val concat : t -> Id.t * Type.t -> t -> t
 val align : int -> int
