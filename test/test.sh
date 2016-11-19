a3ece61e9c3f9389d99e2297f1e08216a20f58c0
wkwkes <oshi.hiroki@gmail.com>
2016-11-20 00:33:00 +0900

    Modify test script

--- test/test.sh
+++ test/test.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
-cd test
 
+# Run this script at /min-caml directory 
+
+# build min-caml
+./to_minips
+make min-caml
+
+# build simulator
+cd test/simulator
+make
+cd ..
+
+# do tests
 for src in *.ml
 do
   echo $src && echo
   name=${src%.ml}
   echo $name
   ../min-caml $name
-  ../../simulator/simulator "${name}.s" > "${name}.res"
+  simulator/simulator "${name}.s" > "${name}.res"
   ocamlopt -w -3-24-26 -o $name $src
   ./$name > "${name}.ans" 
   diff "${name}.ans" "${name}.res" > "${name}.cmp"
@@ -15,8 +26,11 @@ do
   rm $name
   rm "${name}.s"
 done
+
+# remove temporary files
 rm *.cm*
 rm *.o
 rm *.ans
 rm *.res
+
 echo "Test Passed"
