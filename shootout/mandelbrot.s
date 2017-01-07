8bdd4eddbc544d6e94f64b5f5ffcfe53123c991f
wkwkes <oshi.hiroki@gmail.com>
2017-01-07 15:42:12 +0900

    add

--- shootout/mandelbrot.s
+++ shootout/mandelbrot.s
@@ -23,23 +23,23 @@ BEQ_else.117:
 	SUB.s	%f3, %f3, %f4
 	ADD.s	%f3, %f3, %f5
 	SWC1	%f5, 0(%r30)
-	SW	%r1, 8(%r30)
-	SWC1	%f3, 16(%r30)
-	SWC1	%f6, 24(%r30)
-	SWC1	%f2, 32(%r30)
+	SW	%r1, 2(%r30)
+	SWC1	%f3, 4(%r30)
+	SWC1	%f6, 6(%r30)
+	SWC1	%f2, 8(%r30)
 	ADDI	%r29, %r31, 0
-	SW	%r29, 44(%r30) # save link register
-	ADDI	%r30, %r30, 48
+	SW	%r29, 14(%r30) # save link register
+	ADDI	%r30, %r30, 15
 	JAL	dbl.38
-	ADDI	%r29, %r0, 48
+	ADDI	%r29, %r0, 15
 	SUB	%r30, %r30, %r29
-	LW	%r29, 44(%r30)
+	LW	%r29, 14(%r30)
 	ADDI	%r31, %r29, 0
-	LWC1	%f2, 32(%r30) # restore2
+	LWC1	%f2, 8(%r30) # restore2
 	MUL.s	%f1, %f1, %f2
-	LWC1	%f6, 24(%r30) # restore2
+	LWC1	%f6, 6(%r30) # restore2
 	ADD.s	%f2, %f1, %f6
-	LWC1	%f1, 16(%r30) # restore2
+	LWC1	%f1, 4(%r30) # restore2
 	MUL.s	%f3, %f1, %f1
 	MUL.s	%f4, %f2, %f2
 	ADD.s	%f5, %f3, %f4
@@ -47,7 +47,7 @@ BEQ_else.117:
 	LWC1	%f7, 0(%r29)
 	C.lt.s	%r27, %f7, %f5
 	BNE	%r27, %r0, BEQ_else.119
-	LW	%r1, 8(%r30) # restore1
+	LW	%r1, 2(%r30) # restore1
 	ADDI	%r1, %r1, -1
 	LWC1	%f5, 0(%r30) # restore2
 	J	iloop.54
@@ -61,22 +61,22 @@ xloop.44:
 	JR	%r31
 BEQ_else.120:
 	SW	%r1, 0(%r30)
-	SW	%r2, 4(%r30)
+	SW	%r2, 1(%r30)
 	ADDI	%r29, %r31, 0
-	SW	%r29, 12(%r30) # save link register
-	ADDI	%r30, %r30, 16
+	SW	%r29, 6(%r30) # save link register
+	ADDI	%r30, %r30, 7
 	JAL	min_caml_float_of_int
-	ADDI	%r29, %r0, 16
+	ADDI	%r29, %r0, 7
 	SUB	%r30, %r30, %r29
-	LW	%r29, 12(%r30)
+	LW	%r29, 6(%r30)
 	ADDI	%r31, %r29, 0
 	ADDI	%r29, %r31, 0
-	SW	%r29, 12(%r30) # save link register
-	ADDI	%r30, %r30, 16
+	SW	%r29, 6(%r30) # save link register
+	ADDI	%r30, %r30, 7
 	JAL	dbl.38
-	ADDI	%r29, %r0, 16
+	ADDI	%r29, %r0, 7
 	SUB	%r30, %r30, %r29
-	LW	%r29, 12(%r30)
+	LW	%r29, 6(%r30)
 	ADDI	%r31, %r29, 0
 	LA	%r29, l.99
 	LWC1	%f2, 0(%r29)
@@ -84,23 +84,23 @@ BEQ_else.120:
 	LA	%r29, l.100
 	LWC1	%f2, 0(%r29)
 	SUB.s	%f1, %f1, %f2
-	LW	%r1, 4(%r30) # restore1
-	SWC1	%f1, 8(%r30)
+	LW	%r1, 1(%r30) # restore1
+	SWC1	%f1, 2(%r30)
 	ADDI	%r29, %r31, 0
-	SW	%r29, 20(%r30) # save link register
-	ADDI	%r30, %r30, 24
+	SW	%r29, 8(%r30) # save link register
+	ADDI	%r30, %r30, 9
 	JAL	min_caml_float_of_int
-	ADDI	%r29, %r0, 24
+	ADDI	%r29, %r0, 9
 	SUB	%r30, %r30, %r29
-	LW	%r29, 20(%r30)
+	LW	%r29, 8(%r30)
 	ADDI	%r31, %r29, 0
 	ADDI	%r29, %r31, 0
-	SW	%r29, 20(%r30) # save link register
-	ADDI	%r30, %r30, 24
+	SW	%r29, 8(%r30) # save link register
+	ADDI	%r30, %r30, 9
 	JAL	dbl.38
-	ADDI	%r29, %r0, 24
+	ADDI	%r29, %r0, 9
 	SUB	%r30, %r30, %r29
-	LW	%r29, 20(%r30)
+	LW	%r29, 8(%r30)
 	ADDI	%r31, %r29, 0
 	LA	%r29, l.99
 	LWC1	%f2, 0(%r29)
@@ -117,18 +117,18 @@ BEQ_else.120:
 	LWC1	%f3, 0(%r29)
 	LA	%r29, l.102
 	LWC1	%f4, 0(%r29)
-	LWC1	%f5, 8(%r30) # restore2
+	LWC1	%f5, 2(%r30) # restore2
 	ADDI	%r29, %r31, 0
-	SW	%r29, 20(%r30) # save link register
-	ADDI	%r30, %r30, 24
+	SW	%r29, 8(%r30) # save link register
+	ADDI	%r30, %r30, 9
 	JAL	iloop.54
-	ADDI	%r29, %r0, 24
+	ADDI	%r29, %r0, 9
 	SUB	%r30, %r30, %r29
-	LW	%r29, 20(%r30)
+	LW	%r29, 8(%r30)
 	ADDI	%r31, %r29, 0
 	LW	%r1, 0(%r30) # restore1
 	ADDI	%r1, %r1, 1
-	LW	%r2, 4(%r30) # restore1
+	LW	%r2, 1(%r30) # restore1
 	J	xloop.44
 yloop.40:
 	ADDI	%r27, %r0, 400
@@ -142,12 +142,12 @@ BEQ_else.122:
 	ADDI	%r25, %r2, 0 # args
 	ADDI	%r2, %r1, 0 # args
 	ADDI	%r1, %r25, 0 # args
-	SW	%r29, 4(%r30) # save link register
-	ADDI	%r30, %r30, 8
+	SW	%r29, 5(%r30) # save link register
+	ADDI	%r30, %r30, 6
 	JAL	xloop.44
-	ADDI	%r29, %r0, 8
+	ADDI	%r29, %r0, 6
 	SUB	%r30, %r30, %r29
-	LW	%r29, 4(%r30)
+	LW	%r29, 5(%r30)
 	ADDI	%r31, %r29, 0
 	LW	%r1, 0(%r30) # restore1
 	ADDI	%r1, %r1, 1
@@ -159,10 +159,10 @@ _min_caml_start: # main entry point
 	ADDI	%r1, %r0, 0
 	ADDI	%r29, %r31, 0
 	SW	%r29, 4(%r30) # save link register
-	ADDI	%r30, %r30, 8
+	ADDI	%r30, %r30, 5
 	JAL	yloop.40
-	ADDI	%r29, %r0, 8
+	ADDI	%r29, %r0, 5
 	SUB	%r30, %r30, %r29
 	LW	%r29, 4(%r30)
 	ADDI	%r31, %r29, 0
-   # main program end
+halt   # main program end
