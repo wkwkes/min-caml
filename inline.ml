cdff931674df19542341e85fbca2012d7089d5a8
wkwkes <oshi.hiroki@gmail.com>
2017-02-10 15:37:07 +0900

    add a restriction of inline expansions of recursive functionis

--- inline.ml
+++ inline.ml
@@ -1,7 +1,7 @@
 open KNormal
 
-(* インライン展開する関数の最大サイズ (caml2html: inline_threshold) *)
-let threshold = ref 0 (* Mainで-inlineオプションによりセットされる *)
+let threshold = ref 100
+let recThreshold = ref 30
 
 let rec size = function
   | IfEq(_, _, e1, e2) | IfLE(_, _, e1, e2)
@@ -9,24 +9,24 @@ let rec size = function
   | LetTuple(_, _, e) -> 1 + size e
   | _ -> 1
 
-let rec g env = function (* インライン展開ルーチン本体 (caml2html: inline_g) *)
+let rec g env = function
   | IfEq(x, y, e1, e2) -> IfEq(x, y, g env e1, g env e2)
   | IfLE(x, y, e1, e2) -> IfLE(x, y, g env e1, g env e2)
   | Let(xt, e1, e2) -> Let(xt, g env e1, g env e2)
-  | LetRec({ name = (x, t); args = yts; body = e1 }, e2) -> (* 関数定義の場合 (caml2html: inline_letrec) *)
-      let env = if size e1 > !threshold then env else M.add x (yts, e1) env in
+  | LetRec({ name = (x, t); args = yts; body = e1 }, e2) -> (* 鐃舜随申鐃緒申鐃緒申鐃塾常申鐃緒申 (caml2html: inline_letrec) *)
+      let env = if (size e1 > !threshold) && (!recThreshold > 0) then env else M.add x (yts, e1) env in
       LetRec({ name = (x, t); args = yts; body = g env e1}, g env e2)
-  | App(x, ys) when M.mem x env -> (* 関数適用の場合 (caml2html: inline_app) *)
+  | App(x, ys) when M.mem x env ->
       let (zs, e) = M.find x env in
       Format.eprintf "inlining %s@." x;
       let env' =
-	List.fold_left2
-	  (fun env' (z, t) y -> M.add z y env')
-	  M.empty
-	  zs
-	  ys in
+        List.fold_left2
+          (fun env' (z, _) y -> M.add z y env')
+          M.empty
+          zs
+          ys in
       Alpha.g env' e
   | LetTuple(xts, y, e) -> LetTuple(xts, y, g env e)
   | e -> e
 
-let f e = g M.empty e
+let f e = (recThreshold := !recThreshold - 1); g M.empty e
